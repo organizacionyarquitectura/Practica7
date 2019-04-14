a563e0ed48e9ec7b5c545b432d48d5cb1ef9b3a6
SandradelMarSoto <sandradelmarsoto@ciencias.unam.mx>
2019-04-14 02:55:54 -0500

    comentarios corregidos

--- mips/ejercicio02.asm
+++ mips/ejercicio02.asm
@@ -56,9 +56,9 @@ main: 	# Preambulo main
 
 mist_1: # Preambulo mist_1
 	subi	$sp, $sp, 40	# 1 Reservar memoria para el marco
-	sw	$s0, 16($sp)	# 3 Guardar $ra
-	sw	$ra, 20($sp)	# 4 Guardar $fp
-	sw	$fp, 24($sp)
+	sw	$s0, 16($sp)	# 2 Almacenar registros guardados
+	sw	$ra, 20($sp)	# 3 Guardar $ra
+	sw	$fp, 24($sp)	# 4 Establecer $fp
 	addi	$fp, $sp, 36	# 4 Establecer $fp
 	
 	
@@ -68,28 +68,28 @@ mist_1: # Preambulo mist_1
 
 loop_1: beqz $s0, end_1
 	# Invocación de mist_0
-	sw	$t0, 28($sp) # Guardando registros temporales
-	sw	$t1, 32($sp) # Guardando registros temporales
+	sw	$t0, 28($sp) 	# Guardando registros temporales
+	sw	$t1, 32($sp) 	# Guardando registros temporales
 	
-	move 	$a0, $t0 # Se pasa el argumento $a0
-	move 	$a1, $t1 # Se pasa el argumento $a1
-	jal	mist_0	# 3 Brincar a subrutina
+	move 	$a0, $t0 	# Se pasa el argumento $a0
+	move 	$a1, $t1 	# Se pasa el argumento $a1
+	jal	mist_0		# 3 Brincar a subrutina
 	
 	# Retorno de mist_0
-	lw	$t0, 28($sp)	# Restaurar $s0 con el valor de a
-	lw	$t1, 32($sp)	# Restaurar $t0 con el valor de b
+	lw	$t0, 28($sp)	# Restaurar $t0 con el valor de a
+	lw	$t1, 32($sp)	# Restaurar $t1 con el valor de b
 	
 	move $t1, $v0
 	subi $s0, $s0, 1
 	j loop_1
 	
 end_1: # Conclusion mist_1
-	move $v0, $t1 # Se retorna el resultado en $v0
-	lw	$s0, 16($sp)	# 3 Restaurar $ra
-	lw	$ra, 20($sp)	# 4 Restaurar $fp
+	move 	$v0, $t1 	# 1 Se retorna el resultado en $v0
+	lw	$s0, 16($sp)	# 2 Restaurar registros guardados
+	lw	$ra, 20($sp)	
 	lw 	$fp, 24($sp)
-	addi	$sp, $sp, 40
-	jr	$ra
+	addi	$sp, $sp, 40	# 3 Eliminar el marco
+	jr	$ra		# 4 Regresar el control a la rutina invocador
 	
 	#Para este punto, la rutina ya termino, por lo que lo siguiente no se ejecuta
 	# mist_0 recibe como argumentos $a0 y $a1
@@ -103,7 +103,7 @@ mist_0: # Preambulo mist_0
 	addi	$fp, $sp, 28	# 4 Establecer $fp
 	
 	mult $a0, $a1
-	mflo $v0 # Se retorna el resultado en $v0
+	mflo $v0 		# Se retorna el resultado en $v0
 	
 	# Conclusion mist_0
 	lw	$ra, 16($sp)	# 3 Restaurar $ra
